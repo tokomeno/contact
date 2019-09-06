68802f1e8ef01209f6e2ac4e479ccd50c6982b24
Tornike Menabde <tokamenabde@gmail.com>
2019-09-07 03:07:39 +0400

    500 contact

--- database/seeds/ContactSeeder.php
+++ database/seeds/ContactSeeder.php
@@ -12,6 +12,6 @@ class ContactSeeder extends Seeder
      */
     public function run()
     {
-        factory(Contact::class, 100)->create();
+        factory(Contact::class, 500)->create();
     }
 }
